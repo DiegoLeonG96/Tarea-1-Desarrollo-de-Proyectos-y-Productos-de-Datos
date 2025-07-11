d03615a4bbd14cf3525a313023e22d07a64efc33
DiegoLeonG96 <leongonzalez.diego@gmail.com>
2025-07-11 03:13:45 -0400

    se modifica que lea dinámicamente el mes de entrenamiento

--- src/modeling/train.py
+++ src/modeling/train.py
@@ -1,9 +1,20 @@
 from sklearn.ensemble import RandomForestClassifier
 import pandas as pd
-from src.config import FEATURES, TARGET_COL, MODEL_PARAMETERS
+import joblib
+from src.config import FEATURES, TARGET_COL, MODEL_PARAMETERS, MODEL_DIR, PROC_DATA_DIR
+from src.config import TRAINING_MONTH, REDUCE_TRAINING
+import os
 
-def fit_model(taxi_train: pd.DataFrame):
+def fit_model():
+    taxi_train = pd.read_parquet(f'{PROC_DATA_DIR}/processed_data_{TRAINING_MONTH}.parquet')
+    if REDUCE_TRAINING:
+        taxi_train = taxi_train.head(100000)
     rfc = RandomForestClassifier(n_estimators=MODEL_PARAMETERS["n_estimators"], 
                                 max_depth=MODEL_PARAMETERS["max_depth"])
     rfc.fit(taxi_train[FEATURES], taxi_train[TARGET_COL])
+    os.makedirs(MODEL_DIR, exist_ok=True)
+    joblib.dump(rfc, f"{MODEL_DIR}/random_forest.joblib")
     return rfc
+
+if __name__ == '__main__':
+    _ = fit_model()
